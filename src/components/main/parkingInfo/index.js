a0def2ede6b94f1dd6c7ceba32ab1bd6867d9ad4
Kevin Martinez <kmartineze@ucenfotec.ac.cr.com>
2018-09-04 16:58:51 -0600

    Add buttons in parking info cards

--- src/components/main/parkingInfo/index.js
+++ src/components/main/parkingInfo/index.js
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 import getOneUser from "../../../redux/actionsCreators/getOneUser";
 import { Card, Button, CardTitle, CardText, Row, Col } from "reactstrap";
 
+import './parking-info-styles.css';
+
 class ParkingInfo extends React.Component {
   constructor(props) {
     super(props);
@@ -29,8 +31,8 @@ class ParkingInfo extends React.Component {
     const { space } = this.props;
     return (
       <React.Fragment>
-        <div className="col-md-6">
-          <Card body>
+        <div className="col-md-12">
+          <Card body className="card__parking-info">
             <CardTitle>Parking space code {space.code}</CardTitle>
             <CardText>
               Owner: {this.props.owner.name} {this.props.owner.lastName}  
@@ -46,6 +48,15 @@ class ParkingInfo extends React.Component {
             </CardText>
             <CardText>
               to (final date): {this.state.finalDate}
+            </CardText>
+            <CardText>
+              <button type="button" className="btn parking-info__button-primary">
+                Reserve
+              </button>
+
+              <button type="button" className="btn parking-info__button-sharable">
+                Share
+              </button>
             </CardText>
           </Card>
         </div>
